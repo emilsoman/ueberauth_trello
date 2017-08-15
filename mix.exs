dccce944c76beb21eb65343c31cad2a5b368e01b
Will Mernagh <wmernagh@gmail.com>
2017-08-15 01:21:20 -0400

    bump poison

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule UeberauthTrello.Mixfile do
     [
      {:httpoison, "~> 0.7"},
      {:oauther, "~> 1.1"},
-     {:poison, "~> 1.3 or ~> 2.0"},
+     {:poison, "~> 3.0"},
      {:ueberauth, "~> 0.2"},
 
      # dev/test dependencies
